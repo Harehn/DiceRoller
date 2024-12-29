f41bba39ce1a425dfd8fe2dd38a49dd8a7387e68
Nitin <harehn.kaundun@mail.mcgill.ca>
2024-12-29 13:39:51 -0500

    Making an easy roller with api call to backend

--- Frontend/diceroller/src/components/HelloWorld.vue
+++ Frontend/diceroller/src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+  <div>
+  <label for='dice_input'>Dice: </label>
+  <input type='text' id='dice_input' name='dice_input'>
+  <input type='button' value='Roll' @click='writeText'>
+  </div>
+
+  <div v-if='rolled'>
+  <p v-for="result in results">{{result}}</p>
+  </div>
+
   </div>
 </template>
 
@@ -9,6 +20,27 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data (){
+   return {
+    members: [],
+    results: [],
+    rolled: false
+   }
+  },
+  methods: {
+    async writeText() {
+      this.rolled = true;
+      var diceType = document.getElementById("dice_input").value;
+      //https://diceroller-uwe7.onrender.com/roll?roll=33
+      var currResult = await fetch(`https://diceroller-uwe7.onrender.com/roll?roll=${diceType}`)
+      .then(res => res.json())
+      .then(response => response['result'])
+      .catch(err => console.log(err));
+      this.results.unshift(currResult)
+    }
+  },
+  mounted() {
   }
 }
 </script>

8834cfece07f3dfa22b8b461223ffc4b728b597d
Nitin <harehn.kaundun@mail.mcgill.ca>
2024-12-29 17:26:44 -0500

    Added script file to run both servers on development mode

---
+++ run_dev.bat
@@ -0,0 +1,11 @@
+:: Thanks to this link
+:: https://stackoverflow.com/a/28421514/28974846
+start cmd.exe /k .\.venv\Scripts\python.exe .\Backend\server.py
+cd Frontend/diceroller
+start cmd.exe /c npm run serve
+cd ..
+cd ..
+timeout -t 10
+start firefox -new-window http://127.0.0.1:5000
+timeout -t 10
+start firefox http://127.0.0.1:8080

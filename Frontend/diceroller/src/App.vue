dbc9c2c69b5ab65f9b910f515e74c8ea4f43781c
Nitin <harehn.kaundun@mail.mcgill.ca>
2024-12-30 20:13:15 -0500

    Added Navbar interface (no functionality yet)

--- Frontend/diceroller/src/App.vue
+++ Frontend/diceroller/src/App.vue
@@ -1,4 +1,13 @@
 <template>
+  <div class="nav">
+  <ul>
+    <li><a>Home</a></li>
+    <li><a>News</a></li>
+    <li><a>Contact</a></li>
+    <li><a>About</a></li>
+  </ul>
+  </div>
+  
   <HelloWorld msg="Welcome to DiceRoller"/>
 </template>
 
@@ -22,4 +31,38 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+
+ul {
+  list-style-type: none;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+} 
+
 </style>
--- Frontend/diceroller/src/components/HelloWorld.vue
+++ Frontend/diceroller/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
 
   <div>
   <label for='dice_input'>Dice: </label>
